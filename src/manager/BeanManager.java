2cfdf76ed90026c8426f2eb93cff105c2008f6a2
Joao Santana <wozefacu@cartelera.org>
2017-12-16 12:16:29 -0200

    criando projeto mongo

--- src/manager/BeanManager.java
+++ src/manager/BeanManager.java
@@ -4,11 +4,15 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
 import entity.Pessoa;
 import persistence.PessoaDao;
 
+@ManagedBean(name="mb")
+@RequestScoped
 public class BeanManager implements Serializable {
 
 	private static final long serialVersionUID = 1L;
